c1a4b7d37ccbb68b31498e41fe5f4de2e04a8bf9
Agustin Aiello <agustin@android-a8c4ad252cdd83d8.localdomain>
2024-09-11 15:11:24 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="mt-20 border-t py-10 border-neutral-700">
+     
+  
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/constants/index.jsx
@@ -0,0 +1,52 @@
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+
+export const navItems = [
+  { label: "Inicio", href: "#" },
+  { label: "Sobre nosotros", href: "#" },
+  { label: "Contacto", href: "#" },
+];
+
+export const testimonials = [
+  {
+    user: "Agustin Aiello",
+    company: "Founder",
+    image: user1,
+    text: "Técnico Universitario en Programación, desarrollador Fullstack y especialista en aplicaciones multiplataforma.",
+  },
+  {
+    user: "Luciano Escalante",
+    company: "Co-Founder",
+    image: user2,
+    text: "Estudiante de Ingeniería en Informática, programador frontend y especialista en inteligencia artificial.",
+  }
+];
+
+export const checklistItems = [
+  {
+    title: "Desarrollo personalizado",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Compatibililidad Multi-Plataforma",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Soluciones en la Nube",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+  {
+    title: "Escalabilidad a futuro",
+    description:
+      "Track the performance of your VR apps and gain insights into user behavior.",
+  },
+];
+
+
+
+
+
+
